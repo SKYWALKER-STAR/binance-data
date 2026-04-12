04e463331e69121ef155cb5038e8f039f72608fa
mingme47 <mingme47@gmail.com>
2026-04-12 17:35:11 +0800

    feat: 更新启动脚本命令

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+
+source /etc/profile
+proxy_on
+
 #nohup /warehouse/GitRepos/biannce-api/binance/bin/python -m binance_toolkit --config config.json collect > nohup.out 2>&1 &
-nohup /warehouse/GitRepos/biannce-api/binance/bin/python -m binance_toolkit --config config.json collect-mark > nohup.out 2>&1 &
+#nohup /warehouse/GitRepos/biannce-api/binance/bin/python -m binance_toolkit --config config.json collect-mark > nohup.out 2>&1 &
+#nohup /warehouse/GitRepos/biannce-api/binance/bin/python -m binance_toolkit --config config.json ws-mark-price-coin --write-kafka --sample-interval 60 > nohup-lastfundingrate-coin.out 2>&1 &
+nohup /warehouse/GitRepos/biannce-api/binance/bin/python -m binance_toolkit --config config.json ws-mark-price-usdt --write-kafka --sample-interval 60 > nohup-lastfundingrate-usdt.out 2>&1 &
